a5595e95d9f77164eb35e728cc49816e007b478b
humaidi <humaidizakaria1998@gmail.com>
2022-09-16 08:41:14 +0700

    membuat constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,13 @@
 // ENDPOINT
-const baseURL = 'http://127.0.0.1:8000/api/';
-const loginURL = 'http://127.0.0.1:8000/api/';
+const baseURL = 'http://127.0.0.1:8000/api';
+const loginURL = baseURL + '/login';
+const registerURL = baseURL + '/register';
+const logoutURL = baseURL + '/logout';
+const userURL = baseURL + '/user';
+const postURL = baseURL + '/posts';
+const commentsURL = baseURL + '/comments';
+
+// errors
+const serverError = 'Server error';
+const unauthorized = 'Unauthorized';
+const somethingWentWrong = 'Someting went wrong, try again';
