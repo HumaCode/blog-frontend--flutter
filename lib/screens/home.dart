b2f5ca24c1e262e3047b1ab97dacc50deebd42cd
humaidi <humaidizakaria1998@gmail.com>
2022-09-17 14:21:22 +0700

    desain ui register page & function register, logout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_laravel/screens/login.dart';
+import 'package:flutter_blog_laravel/services/user_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,8 +8,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            logout().then((value) => {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                      (route) => false),
+                });
+          },
+          child: const Text('Home Page : Press to logout'),
+        ),
       ),
     );
   }
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_laravel/constants.dart';
+import 'package:flutter_blog_laravel/models/api_response.dart';
+import 'package:flutter_blog_laravel/models/user.dart';
+import 'package:flutter_blog_laravel/screens/home.dart';
+import 'package:flutter_blog_laravel/screens/login.dart';
+import 'package:flutter_blog_laravel/services/user_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -8,11 +15,120 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  //
+  final GlobalKey<FormState> formkey = GlobalKey();
+  bool loading = false;
+  TextEditingController nameController = TextEditingController(),
+      emailController = TextEditingController(),
+      passwordController = TextEditingController(),
+      passwordConfirmController = TextEditingController();
+
+  // panggil function register user
+  void _registerUser() async {
+    ApiResponse response = await registerUser(
+        nameController.text, emailController.text, passwordController.text);
+
+    // jika tidak error maka
+    if (response.error == null) {
+      _saveAndRedirectToHome(response.data as User);
+    } else {
+      setState(() {
+        loading = !loading;
+      });
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('${response.error}')));
+    }
+  }
+
+  void _saveAndRedirectToHome(User user) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setString('token', user.token ?? '');
+    await pref.setInt('user_id', 0);
+
+    // redirect to home
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+        (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Register Page'),
+        title: const Text('Register'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(32),
+          children: [
+            TextFormField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              keyboardType: TextInputType.text,
+              controller: nameController,
+              validator: (val) => val!.isEmpty ? 'Invalid name' : null,
+              decoration: kInputDecoration('Name'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              keyboardType: TextInputType.emailAddress,
+              controller: emailController,
+              validator: (val) => val!.isEmpty ? 'Invalid email address' : null,
+              decoration: kInputDecoration('Email'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              controller: passwordController,
+              obscureText: true,
+              validator: (val) =>
+                  val!.length < 6 ? 'Required at least 6 chars' : null,
+              decoration: kInputDecoration('Password'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              controller: passwordConfirmController,
+              obscureText: true,
+              validator: (val) => val != passwordController.text
+                  ? 'Confirm password does not match'
+                  : null,
+              decoration: kInputDecoration('Confirm password'),
+            ),
+            const SizedBox(height: 10),
+            loading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : kTextButton('Register', () {
+                    if (formkey.currentState!.validate()) {
+                      setState(() {
+                        loading = !loading;
+                        _registerUser();
+                      });
+                    }
+
+                    // debugPrint(txtPassword.text);
+                  }),
+            const SizedBox(height: 10),
+            kLoginRegister('Already have an account? ', 'Login', () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                  (route) => false);
+            })
+          ],
+        ),
       ),
     );
   }
