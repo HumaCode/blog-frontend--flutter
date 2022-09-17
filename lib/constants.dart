c0b23f4bf3ef160d4f5002d8f54fdb96f8ed1a7d
humaidi <humaidizakaria1998@gmail.com>
2022-09-17 13:47:33 +0700

    desain ui login page & function login

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,13 +1,67 @@
 // ENDPOINT
-const baseURL = 'http://127.0.0.1:8000/api';
-const loginURL = baseURL + '/login';
-const registerURL = baseURL + '/register';
-const logoutURL = baseURL + '/logout';
-const userURL = baseURL + '/user';
-const postURL = baseURL + '/posts';
-const commentsURL = baseURL + '/comments';
+import 'package:flutter/material.dart';
+
+const baseURL = 'https://tes.sipandawa.com/public/api';
+const loginURL = '$baseURL/login';
+const registerURL = '$baseURL/register';
+const logoutURL = '$baseURL/logout';
+const userURL = '$baseURL/user';
+const postURL = '$baseURL/posts';
+const commentsURL = '$baseURL/comments';
 
 // errors
 const serverError = 'Server error';
 const unauthorized = 'Unauthorized';
 const somethingWentWrong = 'Someting went wrong, try again';
+
+// inputdecoration
+InputDecoration kInputDecoration(String label) {
+  return InputDecoration(
+    labelText: label,
+    contentPadding: const EdgeInsets.all(10),
+    border: const OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 1,
+        color: Colors.black,
+      ),
+    ),
+  );
+}
+
+// textbutton
+TextButton kTextButton(String label, Function onPressed) {
+  return TextButton(
+    onPressed: () => onPressed(),
+    child: Text(
+      label,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+    style: ButtonStyle(
+      backgroundColor: MaterialStateColor.resolveWith((states) => Colors.blue),
+      padding: MaterialStateProperty.resolveWith(
+        (states) => const EdgeInsets.symmetric(vertical: 10),
+      ),
+    ),
+  );
+}
+
+// login register
+Row kLoginRegister(String text, String label, Function onTap) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(text),
+      GestureDetector(
+        child: Text(
+          label,
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+        onTap: () => onTap(),
+      )
+    ],
+  );
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,13 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_laravel/constants.dart';
+import 'package:flutter_blog_laravel/models/api_response.dart';
+import 'package:flutter_blog_laravel/models/user.dart';
+import 'package:flutter_blog_laravel/screens/home.dart';
+import 'package:flutter_blog_laravel/screens/register.dart';
+import 'package:flutter_blog_laravel/services/user_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+  bool loading = false;
+
+  // panggil function login user
+  void _loginUser() async {
+    ApiResponse response = await login(txtEmail.text, txtPassword.text);
+
+    // jika tidak error maka
+    if (response.error == null) {
+      _saveAndRedirectToHome(response.data as User);
+    } else {
+      setState(() {
+        loading = false;
+      });
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('${response.error}')));
+    }
+  }
+
+  void _saveAndRedirectToHome(User user) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setString('token', user.token ?? '');
+    await pref.setInt('user_id', 0);
+
+    // redirect to home
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+        (route) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: const Text('Login'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(32),
+          children: [
+            TextFormField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              keyboardType: TextInputType.emailAddress,
+              controller: txtEmail,
+              validator: (val) => val!.isEmpty ? 'Invalid email address' : null,
+              decoration: kInputDecoration('Email'),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              controller: txtPassword,
+              obscureText: true,
+              validator: (val) =>
+                  val!.length < 6 ? 'Required at least 6 chars' : null,
+              decoration: kInputDecoration('password'),
+            ),
+            const SizedBox(height: 10),
+            loading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : kTextButton('Login', () {
+                    if (formkey.currentState!.validate()) {
+                      setState(() {
+                        loading = true;
+                        _loginUser();
+                      });
+                    }
+
+                    // debugPrint(txtPassword.text);
+                  }),
+            const SizedBox(height: 10),
+            kLoginRegister('Dont have an account? ', 'Register', () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterPage()),
+                  (route) => false);
+            })
+          ],
+        ),
       ),
     );
   }
