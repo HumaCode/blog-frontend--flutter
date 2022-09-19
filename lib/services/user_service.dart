f287cce27f6b4146160f73ed5dddb0b23d1311e6
humaidi <humaidizakaria1998@gmail.com>
2022-09-19 14:19:48 +0700

    membuat function edit  profil

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -105,6 +105,40 @@ Future<ApiResponse> getUserDetail() async {
   return apiResponse;
 }
 
+// update user
+Future<ApiResponse> updateUser(String name, String? image) async {
+  ApiResponse apiResponse = ApiResponse();
+
+  try {
+    String token = await getToken();
+    final response = await http.put(Uri.parse(userURL),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+        body: image == null
+            ? {
+                'name': name,
+              }
+            : {'name': name, 'image': image});
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWentWrong;
+        break;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
 // get token
 Future<String> getToken() async {
   SharedPreferences pref = await SharedPreferences.getInstance();
@@ -112,7 +146,7 @@ Future<String> getToken() async {
   return pref.getString('token') ?? '';
 }
 
-// get user by id
+// get user by id shared preferences
 Future<int> getUserId() async {
   SharedPreferences pref = await SharedPreferences.getInstance();
   return pref.getInt('userId') ?? 0;
